bc2ddf71aafa25a5047953156b91b5bffa5ea791
parsa222 <70150489+parsa222@users.noreply.github.com>
2026-07-28 22:18:49 -0700

    syntax fix 2.0 ( shellcheck CCOPT )

--- gentables.sh
+++ gentables.sh
@@ -3,7 +3,7 @@
 CC=${CC:=cc}
 CCOPT="-Wall -W -O2"
 
-$CC gentables.c -o gentables $CCOPT
-./gentables > tables.c
-./gentables h > tables.h
+${CC} gentables.c -o gentables ${CCOPT}
+./gentables >tables.c
+./gentables h >tables.h
 echo Tables generated
